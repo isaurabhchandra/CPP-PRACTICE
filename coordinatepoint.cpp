4917d75533af47c58a44855eaa4f673bbddbc089
isaurabhchandra <111374947+isaurabhchandra@users.noreply.github.com>
2022-11-22 19:08:11 +0530

    Create coordinatepoint.cpp

---
+++ coordinatepoint.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h> 
+#include <iostream>
+using namespace std;
+
+#include <bits/stdc++.h> 
+#include <iostream>
+using namespace std;
+
+int main() {
+   int x,y;
+    cin>>x>>y;
+   
+     if(x>0 && y>0){
+      cout <<"1st Quadrant"<<endl;
+    }
+    else if(x<0 && y>0){
+      cout <<"2nd Quadrant"<<endl;  
+    }
+    else if(x<0 && y<0){
+        cout<<"3rd Quadrant"<<endl;
+    }
+    else if(x>0 && y<0){
+       cout<<"4th Quadrant"<<endl; 
+    }
+    else if(x!=0 && y==0){
+  cout<<"x axis"<<endl; 
+
+    }
+     else if(x==0 && y!=0){ 
+  cout<<"y axis"<<endl; 
+     }
+    else
+        cout<<"Origin"<<endl;
+    
+    return 0;
+}
